29fc9137e37535467600cb578b0cf58e87739e98
barroz1996 <62878759+barroz1996@users.noreply.github.com>
2022-11-03 18:10:01 +0200

    add new button to search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
-
+import FeedbackForm from './components/FeedbackForm'
 import { useState } from 'react'
 import FeedbackData from './data/FeedbackData'
 
@@ -18,11 +18,10 @@ function App(){
     <>  
         <Header />
         <div className='container'> 
-        
-        <FeedbackStats feedback = {feedback} />
-        <FeedbackList feedback ={feedback} handleDelete =
-        {deleteFeedback} />
-       
+            <FeedbackForm />
+            <FeedbackStats feedback = {feedback} />
+            <FeedbackList feedback ={feedback} handleDelete =
+            {deleteFeedback} />
         </div> 
     </>   
     )
