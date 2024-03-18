a1453a7af4ee8c6d90b24a87ed03aaf887660781
barroz1996 <62878759+barroz1996@users.noreply.github.com>
2024-03-18 16:20:13 +0200

    add stats, and add delete option

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import Header from "./components/Header"
-import FeedbackItem from "./components/FeedbackItem"
+import FeedbackList from "./components/FeedbackList"
+import { useState } from "react"
+import FeedbackData from "./data/FeedbackData"
+import FeedbackStats from "./components/FeedbackStats"
+
+
 function App(){
+  const[feedback, setFeedback] = useState(FeedbackData)
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure you want to delete?')){
+      setFeedback(feedback.filter ((item)=> item.id !== id)) // we want to filter the id was clicked by the client
+    }
+    
+  }
+
   return(
     <>
-    <Header />
+    <Header feedback = {feedback}/>
       <div className="container">
-        <FeedbackItem />
+        <FeedbackStats feedback={feedback}/>
+        <FeedbackList feedback = {feedback}
+        handleDelete = {deleteFeedback}/>
       </div>
     </>
   )
