b92a9398c1f638eea2b0a53972655f9279a51e4d
barroz1996 <62878759+barroz1996@users.noreply.github.com>
2024-03-22 01:04:04 +0200

    add option of add the rating to the form

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import { useState } from "react"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackFrom from "./components/FeedbackFrom"
+import {v4 as uuidv4} from 'uuid'
+
 
 function App(){
   const[feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback)=>{
+    newFeedback.id = uuidv4() // make the rating we get unseen with code
+      setFeedback([newFeedback, ...feedback ])
+  }
+
   const deleteFeedback = (id) => {
     if(window.confirm('Are you sure you want to delete?')){
       setFeedback(feedback.filter ((item)=> item.id !== id)) // we want to filter the id was clicked by the client
@@ -18,7 +26,7 @@ function App(){
     <>
     <Header feedback = {feedback}/>
       <div className="container">
-        <FeedbackFrom />
+        <FeedbackFrom handleAdd = {addFeedback}/>
         <FeedbackStats feedback={feedback}/>
         <FeedbackList feedback = {feedback}
         handleDelete = {deleteFeedback}/>
