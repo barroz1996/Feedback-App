4c709d4204c8df84cb2ca869fd190714f78609ce
barroz1996 <62878759+barroz1996@users.noreply.github.com>
2024-04-03 18:51:50 +0300

    add spinner componenet

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,9 @@
+import spinner from "../assets/spinner.gif"
+
+function Spinner() {
+   
+    return <img src = { spinner }  alt = "Loading..." style={{width: "100px", margin: "auto", diaplay: "block"}}/>
+  
+}
+
+export default Spinner
